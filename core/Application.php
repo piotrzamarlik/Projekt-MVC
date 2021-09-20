e286d568da469ce3d9767e9b21586eb6447a0d2b
piotrzamarlik <85066588+piotrzamarlik@users.noreply.github.com>
2021-09-20 20:28:04 +0200

    Add files via upload

--- core/Application.php
+++ core/Application.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace app\core;
+
+/**
+ * Class Application
+ */
+class Application
+{
+    public Router $router;
+    /**
+     * Application constructor
+     */
+    public function __construct()
+    {
+        $this->router = new Router();
+    }
+}
