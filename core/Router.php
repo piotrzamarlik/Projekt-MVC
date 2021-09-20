6c7196f2be22aad757c08648e257d68a8a5e207e
piotrzamarlik <85066588+piotrzamarlik@users.noreply.github.com>
2021-09-20 20:28:52 +0200

    Add files via upload

---
+++ core/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Router
+ */
+class Router
+{
+    /**
+     * Router constructor
+     */
+    public function __construct()
+    {
+        
+    }
+}
