ce306315a8d253d80e07850db664e074f2a0912d
piotrzamarlik <85066588+piotrzamarlik@users.noreply.github.com>
2021-09-30 12:48:26 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -12,4 +12,7 @@ $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
 $app->router->get('/contact', 'contact');
+$app->router->post('/contact', function() {
+    
+});
 $app->run();
