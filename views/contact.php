5d49b3fc0656b2a20b71b17b3efbd1a5a3e9e607
piotrzamarlik <85066588+piotrzamarlik@users.noreply.github.com>
2021-09-30 12:40:11 +0200

    Add files via upload

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,17 @@
 <h1>Kontakt</h1>
+
+<form>
+    <div class="form-group">
+        <label class="form-label">Temat</label>
+        <input type="text" name="temat" class="form-control">
+    </div>
+    <div class="form-group">
+        <label class="form-label">Email address</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Treść</label>
+        <textarea name="tresc" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
