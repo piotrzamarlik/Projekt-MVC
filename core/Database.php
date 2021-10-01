4e241276a1c6a4166a472ee11df4af915532325a
piotrzamarlik <piotr.zamarlik@gmail.com>
2021-10-01 15:30:49 +0000

    Wybór nowych migracji

--- core/Database.php
+++ core/Database.php
@@ -17,4 +17,40 @@ class Database
         $this->pdo = new \PDO($dsn, $user, $password);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function applyMigrations()
+    {
+        $this->createMigrationsTable();
+        $migrated = $this->getMigrations();
+
+        $migrations = scandir(Application::$ROOT_DIR . '/migrations');
+        $newMigrations = array_diff($migrations, $migrated);
+
+        foreach ($newMigrations as $migration) {
+            if ($migration === '.' || $migration === '..') {
+                continue;
+            }
+
+            require_once Application::$ROOT_DIR . '/migraitons/' . $migration;
+            $calssName = pathinfo($migration, PATHINFO_FILENAME);
+        }
+    }
+
+    public function createMigrationsTable()
+    {
+        $this->pdo->exec("
+            CREATE TABLE IF NOT EXISTS migrations (
+                id SERIAL PRIMARY KEY,
+                migration VARCHAR(255),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )");
+    }
+
+    public function getMigrations()
+    {
+        $stmt = $this->pdo->prepare("SELECT migration FROM migrations");
+        $stmt->execute();
+
+        return $stmt->fetchAll(\PDO::FETCH_COLUMN);
+    }
 }
